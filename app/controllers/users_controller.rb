47411fb3058108d64ab012c151998acc95e0a769
Bob Khan <shopping_smart_au@outlook.com>
2020-11-08 20:29:31 +0000

    section 144

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
 
 
     def create
